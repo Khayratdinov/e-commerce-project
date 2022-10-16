eeab5144b6508436ae81227159c182ceaf34ac98
Nurpolat <khayratdinov.np@gmail.com>
2022-10-16 16:31:10 +0500

    chore: add func: book list by tag & category

--- project/apps/book/views.py
+++ project/apps/book/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # ============================================================================ #
-from project.apps.book.models import Category, Tag, Book
+from project.apps.book.models import Category, Tag, Book, BookSlider
 
-# Create your views here.
+
+# ============================================================================ #
+#                                     BOOK                                     #
+# ============================================================================ #
 
 
 def book_list(request):
@@ -13,5 +16,26 @@ def book_list(request):
     return render(request, "book/book_list.html", context)
 
 
-def book_detail(request):
-    pass
+def book_detail(request, pk):
+    book = Book.objects.filter(pk=pk)
+    book_slider = BookSlider.objects.filter(book=book)
+
+    context = {"book": book, "book_slider": book_slider}
+
+    return render(request, "book/book_detail.html", context)
+
+
+def book_list_by_category(request, pk):
+    category = Category.objects.filter(pk=pk)
+    books = Book.objects.filter(category=category)
+
+    context = {"category": category, "books": books}
+    return render(request, "book/book_detail.html", context)
+
+
+def book_list_by_tag(request, pk):
+    tag = Tag.objects.filter(pk=pk)
+    books = Book.objects.filter(tags=tag)
+
+    context = {"tag": tag, "books": books}
+    return render(request, "book/book_detail.html", context)
