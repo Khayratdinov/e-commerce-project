323f0d400d83af99d2a61b5640e9262d93053ee8
Nurpolat <khayratdinov.np@gmail.com>
2022-10-17 10:57:13 +0500

    chore: add users app

--- project/settings/base.py
+++ project/settings/base.py
@@ -44,6 +44,7 @@ LOCAL_APPS = [
     "project.apps.core",
     "project.apps.common",
     "project.apps.book",
+    "project.apps.users",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
