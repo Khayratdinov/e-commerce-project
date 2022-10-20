3307f23779ba2341da8b44f01a08fb72c30e461a
Nurpolat <khayratdinov.np@gmail.com>
2022-10-21 00:25:15 +0500

    chore: add common info and Headimages

--- project/apps/common/models.py
+++ project/apps/common/models.py
@@ -1,3 +1,4 @@
+from unittest.mock import Base
 from django.db import models
 
 # ============================================================================ #
@@ -12,6 +13,33 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+# ======================== COMMON INFORMATION WEBSITE ======================== #
+
+
+class CommonInfo(BaseModel):
+
+    STATUS = (
+        ("True", "True"),
+        ("False", "False"),
+    )
+
+    logo = models.ImageField(blank=True, upload_to="images/")
+
+    description_contact = models.TextField(blank=True)
+    description_footer = models.TextField(blank=True)
+
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=50, blank=True)
+
+    telegram = models.CharField(max_length=255, blank=True)
+    instagram = models.CharField(max_length=255, blank=True)
+    facebook = models.CharField(max_length=255, blank=True)
+    status = models.CharField(max_length=15, choices=STATUS, default="False")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
 
 # ================================ HOMESLIDER ================================ #
@@ -33,3 +61,17 @@ class HomeSlider(BaseModel):
 
     def __str__(self):
         return self.title
+
+
+# =============================== HEADER IMAGES ============================== #
+
+
+class HeadImages(BaseModel):
+
+    STATUS = (
+        ("True", "Available"),
+        ("False", "Not Available"),
+    )
+
+    shape = models.ImageField(upload_to="images/")
+    status = models.CharField(max_length=15, choices=STATUS, default="True")
