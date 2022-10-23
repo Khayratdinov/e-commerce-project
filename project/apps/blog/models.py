3327ff092ae441584dfe006ff9b0a0716604685a
Nurpolat <khayratdinov.np@gmail.com>
2022-10-23 11:08:58 +0500

    chore: add models in blog app

--- project/apps/blog/models.py
+++ project/apps/blog/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.db.models import Count
+from django.urls import reverse
 
-# Create your models here.
+# ============================================================================ #
+from project.apps.common.models import BaseModel
+
+# ============================================================================ #
+#                                     BLOG                                     #
+# ============================================================================ #
+
+
+class Blog(BaseModel):
+
+    STATUS = (
+        ("True", "Published"),
+        ("False", "Not Published"),
+    )
+
+    title = models.CharField(max_length=355)
+    image = models.ImageField(upload_to="images/")
+
+    description = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=15, choices=STATUS, default="True")
+    category = models.ForeignKey("Category_Blog", on_delete=models.CASCADE)
+    views = models.PositiveBigIntegerField(default=0)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"slug": self.slug})
+
+
+# ============================================================================ #
+
+
+class Category_Blog(BaseModel):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("category_blog_detail", kwargs={"slug": self.slug})
+
+
+# ============================================================================ #
+
+
+class Comment_blog(BaseModel):
+
+    STATUS = (
+        ("True", "No Block"),
+        ("False", "Block"),
+    )
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=55, blank=False)
+    phone = models.IntegerField(blank=False)
+    email = models.EmailField(blank=True, null=True)
+    comment = models.TextField(max_length=355, blank=False)
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.CharField(max_length=15, choices=STATUS, default="True")
+
+    def __str__(self):
+        return self.name
