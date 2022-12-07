0d2b804844bbc20409703f91de5501ce431ee9d5
khayratdinov <khayratdinov.np@gmail.com>
2022-12-07 15:41:57 +0500

    chore: 🔩 updated tags add recent blog list and blog comment

--- project/apps/common/templatetags/sidebar_blog_tag.py
+++ project/apps/common/templatetags/sidebar_blog_tag.py
@@ -9,3 +9,13 @@ register = template.Library()
 @register.simple_tag(name="get_categories")
 def get_categories():
     return CategoryBlog.objects.all()
+
+
+@register.simple_tag(name="get_blog_recent")
+def get_blog_recent():
+    return Blog.objects.filter(status="True").order_by("-created_at")[:3]
+
+
+@register.simple_tag(name="get_blog_comment")
+def get_blog_comment():
+    return BlogComment.objects.filter(status="True").order_by("-created_at")[:5]
